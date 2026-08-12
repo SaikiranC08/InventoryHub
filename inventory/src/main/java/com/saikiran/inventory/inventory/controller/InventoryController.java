7dd3876138b4d7ce0e9281967512fa7a6b88552e
SaikiranC08 <schevula26@gmail.com>
2026-08-12 22:20:26 +0530

    Test inventory CI/CD pipeline

--- inventory/src/main/java/com/saikiran/inventory/inventory/controller/InventoryController.java
+++ inventory/src/main/java/com/saikiran/inventory/inventory/controller/InventoryController.java
@@ -232,6 +232,6 @@ public class InventoryController {
         return ResponseEntity.ok(orderQueryService.getSuppliersByBusinessId(businessId));
     }
 
-    // Testing CI workflow 3
+    // Testing CI workflow 4
 
 }
